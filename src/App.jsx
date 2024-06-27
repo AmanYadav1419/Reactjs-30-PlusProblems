34fb519feaf78759d97f3e5d86dd04b1665d34e9
unknown <amanyadav16544@gmail.com>
2024-06-27 12:18:06 +0530

    rendered all problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import HelloWorld from "./components/1st-Problem/HelloWorld";
-// import CounterApp from "./components/2nd-problem/CounterApp";
-// import FormInput from "./components/3rd-Problem/FormInput";
-// import ListComponent from "./components/4th-Problem/ListComponent";
+import HelloWorld from "./components/1st-Problem/HelloWorld";
+import CounterApp from "./components/2nd-problem/CounterApp";
+import FormInput from "./components/3rd-Problem/FormInput";
+import ListComponent from "./components/4th-Problem/ListComponent";
 import NavigationMenu from "./components/18th-Problem/NavigationMenu";
 import LocalizationSystem from "./components/26th-Problem/LocalizationSystem";
 import { LocalizationProvider } from "./components/26th-Problem/LocalizationContext";
@@ -10,22 +10,22 @@ import MultipleAPIdataFetch from "./components/27th-Problem/MultipleAPIdataFetch
 import MultipleStateInSingleState from "./components/28th-Problem/MultipleStateInSingleState";
 import ImageGallery from "./components/29th-Problem/ImageGallery";
 import DifferentRoutePages from "./components/30th-Problem/DifferentRoutePages";
-// import DarkandLightTheme from "./components/19th-Problem/DarkandLightTheme";
-// import { ThemeProvider } from "./components/19th-Problem/ThemeContext";
-// import NineteenComp from "./components/19th-Problem/NineteenComp";
-// import ToggleSwitch from "./components/5th-Problem/ToggleSwitch";
-// import FetchDataFromAPI from "./components/6th-Problem/FetchDataFromAPI";
-// import TimerCountDown from "./components/7th-Problem/TimerCountDown";
-// import ToDoListApp from "./components/8th-Problem/ToDoListApp";
-// import BackgroundChange from "./components/9th-Problem/BackgroundChange";
-// import BasicRoute from "./components/10th-Problem/BasicRoute";
-// import RandomQuote from "./components/11th-Problem/RandomQuote";
-// import FileUploader from "./components/12th-Problem/FileUploader";
-// import BasicLoginAndRegistrationForm from "./components/13th-Problem/BasicLoginAndRegistrationForm";
-// import WeatherApp from "./components/14th-Problem/WeatherApp";
-// import SearchBarFilter from "./components/15th-Problem/SearchBarFilter";
-// import PaginationComponent from "./components/16th-Problem/PaginationComponent";
-// import ColorPicker from "./components/17th-Problem/ColorPicker";
+import DarkandLightTheme from "./components/19th-Problem/DarkandLightTheme";
+import { ThemeProvider } from "./components/19th-Problem/ThemeContext";
+import NineteenComp from "./components/19th-Problem/NineteenComp";
+import ToggleSwitch from "./components/5th-Problem/ToggleSwitch";
+import FetchDataFromAPI from "./components/6th-Problem/FetchDataFromAPI";
+import TimerCountDown from "./components/7th-Problem/TimerCountDown";
+import ToDoListApp from "./components/8th-Problem/ToDoListApp";
+import BackgroundChange from "./components/9th-Problem/BackgroundChange";
+import BasicRoute from "./components/10th-Problem/BasicRoute";
+import RandomQuote from "./components/11th-Problem/RandomQuote";
+import FileUploader from "./components/12th-Problem/FileUploader";
+import BasicLoginAndRegistrationForm from "./components/13th-Problem/BasicLoginAndRegistrationForm";
+import WeatherApp from "./components/14th-Problem/WeatherApp";
+import SearchBarFilter from "./components/15th-Problem/SearchBarFilter";
+import PaginationComponent from "./components/16th-Problem/PaginationComponent";
+import ColorPicker from "./components/17th-Problem/ColorPicker";
 import ShoppingCart from "./components/20th-Problem/ShoppingCart";
 import { CartProvider } from "./components/20th-Problem/CartContext";
 import CounterWithReducer from "./components/21th-Problem/CounterWithReducer";
@@ -55,66 +55,66 @@ function App() {
   return (
     <div>
       {/* 1st problem */}
-      {/* <HelloWorld /> */}
+      <HelloWorld />
 
       {/* 2nd problem */}
-      {/* <CounterApp /> */}
+      <CounterApp />
 
       {/* 3rd problem */}
-      {/* <FormInput /> */}
+      <FormInput />
 
       {/* 4th Problem */}
-      {/* <ListComponent /> */}
+      <ListComponent />
 
       {/* 5th Problem */}
-      {/* <ToggleSwitch /> */}
+      <ToggleSwitch />
 
       {/* 6th Problem */}
-      {/* <FetchDataFromAPI /> */}
+      <FetchDataFromAPI />
 
       {/* 7th Problem */}
-      {/* <TimerCountDown /> */}
+      <TimerCountDown />
 
       {/* 8th Problem */}
-      {/* <ToDoListApp /> */}
+      <ToDoListApp />
 
       {/* 9th Problem */}
-      {/* <BackgroundChange /> */}
+      <BackgroundChange />
 
       {/* 10th Problem */}
-      {/* <BasicRoute /> */}
+      <BasicRoute />
 
       {/* 11th Problem */}
-      {/* <RandomQuote /> */}
+      <RandomQuote />
 
       {/* 12th Problem */}
-      {/* <FileUploader /> */}
+      <FileUploader />
 
       {/* 13th Problem */}
-      {/* <BasicLoginAndRegistrationForm /> */}
+      <BasicLoginAndRegistrationForm />
 
       {/* 14th Problem */}
-      {/* <WeatherApp /> */}
+      <WeatherApp />
 
       {/* 15th Problem */}
-      {/* <SearchBarFilter listofNames={listofNames}/> */}
+      <SearchBarFilter listofNames={listofNames}/>
 
       {/* 16th Problem */}
-      {/* <PaginationComponent listofNames={listofNames} itemsPerPage={2}/> */}
+      <PaginationComponent listofNames={listofNames} itemsPerPage={2}/>
 
       {/* 17th Problem */}
-      {/* <ColorPicker /> */}
+      <ColorPicker />
 
       {/* 18th Problem */}
-      {/* <NavigationMenu /> */}
+      <NavigationMenu />
 
       {/* 19th Problem */}
-      {/* <ThemeProvider>
+      <ThemeProvider>
         <div className="App">
           <DarkandLightTheme />
           <NineteenComp />
         </div>
-      </ThemeProvider> */}
+      </ThemeProvider>
 
       {/* 20th Problem */}
       <CartProvider>
